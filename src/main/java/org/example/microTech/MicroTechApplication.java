1abc6bd9f84d3b558ba31982cbf17f6d00f18a4d
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-11-24 21:36:16 +0100

    first commit

---
+++ src/main/java/org/example/microTech/MicroTechApplication.java
@@ -0,0 +1,13 @@
+package org.example.microTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/microTech/MicroTechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
